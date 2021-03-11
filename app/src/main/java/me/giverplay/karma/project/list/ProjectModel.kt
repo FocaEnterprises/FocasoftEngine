a7d9d1d463e3b96319b31558cdaaa0aae8c6bd02
GiverPlay007 <play.giver.007@gmail.com>
2021-03-11 18:36:48 -0300

    Project list on RecyclerView

---
+++ app/src/main/java/me/giverplay/karma/project/list/ProjectModel.kt
@@ -0,0 +1,9 @@
+package me.giverplay.karma.project.list
+
+class ProjectModel(
+    val id: String,
+    val name: String,
+    val description: String,
+    val type: String,
+    val imageIcon: String
+)
