13a0a931637419e73877dd86fb375a45598a0325
GiverPlay007 <play.giver.007@gmail.com>
2021-03-16 22:37:41 -0300

    Migrate to flutter

---
+++ android/app/src/main/kotlin/net/dinastiafoca/focasoft/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.dinastiafoca.focasoft
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- app/src/main/java/me/giverplay/karma/project/list/ProjectModel.kt
+++
@@ -1,9 +0,0 @@
-package me.giverplay.karma.project.list
-
-class ProjectModel(
-    val id: String,
-    val name: String,
-    val description: String,
-    val type: String,
-    val imageIcon: String
-)
